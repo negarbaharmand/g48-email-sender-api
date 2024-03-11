6e0f4949afbc1ae062da5b5974e9bf1f0d0f22c8
Negar Baharmand <negarbaharmand@outlook.com>
2024-03-11 12:01:28 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/g48emailsenderapi/G48EmailSenderApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g48emailsenderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G48EmailSenderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G48EmailSenderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g48emailsenderapi/G48EmailSenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g48emailsenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G48EmailSenderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

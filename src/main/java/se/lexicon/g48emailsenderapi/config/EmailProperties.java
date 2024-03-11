2c7ee87ddfac2b70b017f11ebc14f9bcff748b00
Negar Baharmand <negarbaharmand@outlook.com>
2024-03-11 12:04:02 +0100

    Configure email server

---
+++ src/main/java/se/lexicon/g48emailsenderapi/config/EmailProperties.java
@@ -0,0 +1,25 @@
+package se.lexicon.g48emailsenderapi.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Getter
+@Configuration
+@PropertySource(value = {"classpath:email.properties"})
+public class EmailProperties {
+
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private Integer port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean smtAuth;
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean startTLS;
+}
